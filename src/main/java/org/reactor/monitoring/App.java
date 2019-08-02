14742462bd42f6a2eebafe7854a1c0e79e7e6f2e
Randika-Isuru <randikaisuru@gmail.com>
2019-08-02 11:49:27 +0530

    Initial commit of machine learning repo

---
+++ src/main/java/org/reactor/monitoring/App.java
@@ -0,0 +1,13 @@
+package org.reactor.monitoring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
